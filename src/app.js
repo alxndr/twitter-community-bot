08356c2bf6c86f39f12e3c0bd562f9f2bfac0f10
Alexander <alxndr+github@gmail.com>
2014-01-13 07:56:11 -0800

    WIP database

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+console.log(process.title + ' ' + process.version);
+console.log(process);
+var env = require('node-env-file');
+env('./.env');
+
 var secrets = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -5,16 +10,24 @@ var secrets = {
   access_token_secret: process.env.ACCESS_TOKEN_SECRET
 };
 
-var db = new pg.Client(process.env.DATABASE_URL || 'something').connect();
-var query = client.query('SELECT * FROM mytable');
-query.on('end', function() { db.end(); });
+//console.log('secrets');
+//console.log(secrets);
 
 var TCB = require('./tcb.js');
 var Twit = require('twit');
 var WebServer = require('./server.js');
+var pg = require('pg');
 
-var term = '@drwxrxrx_dev'; // TODO take from env
+var conString = "postgres://postgres:1234@localhost/tcb";
+var client = new pg.Client(conString);
+console.log('client');
+console.log(client);
+console.log('client');
+
+var term = '@drwxrxrx_dev'; // TODO take from env. process.argv[2] or find an options parser
 var twit = new Twit(secrets);
 
-new TCB({ T: twit, term: term }).start();
+new TCB({ T: twit, term: term, db_client: client }).start();
+
 new WebServer().start();
+
--- src/tcb.js
+++ src/tcb.js
@@ -3,18 +3,58 @@ var Tweet = require('./tweet.js');
 var TCB = function(config) {
   this.T = config.T;
   this.term = config.term;
-  this.username = 'drwxrxrx_dev'; // todo find a better way to do this
+  this.db_client = config.db_client;
+  this.username = 'drwxrxrx_dev'; // TODO find a better way to do this
   this.username_regex = new RegExp('^\\s*@' + this.username + '\\s+');
-}
+  if (this.db_client) {
+    this.look_for_tweets();
+  }
+};
+TCB.prototype.look_for_tweets = function() {
+  console.log('connecting');
+  var self = this;
+  this.db_client.connect(function(err) {
+    if (err) { return console.error('could not connect to postgres', err); }
+
+    self.db_client.query('SELECT * from tweets', function(err, result) {
+      if (err) { return console.error('error running query', err); }
+
+      console.log('query end called');
+      if (result.rowCount) {
+        console.log('tweets!');
+        console.log(result);
+      } else {
+        console.log('no tweets stored in db');
+      }
+      self.db_client.end();
+    });
+  });
+};
+TCB.prototype.store_in_db = function(tweet) {
+  var self = this;
+  this.db_client.connect(function(err) {
+    if (err) return console.error('could not connect to postgres', err);
+
+    self.db_client.query(
+      'insert into tweets(author,text,created_at) values($1, $2, $3)',
+      [tweet.user.screen_name, tweet.text, tweet.created_at]
+    ).on('row', function(row) {
+      console.log(row);
+    });
+  });
+};
 TCB.prototype.start = function() {
   var stream = this.T.stream('statuses/filter', { track: this.term, lang: 'en' });
   stream.on('tweet', this.term_mentioned.bind(this));
 };
 TCB.prototype.term_mentioned = function(tweet_json) {
   var tweet = new Tweet(tweet_json);
+
   if (!tweet.is_by(this.username)) {
     this.repost(tweet);
   }
+
+  this.store_in_db(tweet);
 };
 TCB.prototype.repost = function(tweet) {
   var text = tweet.process_text(this.username_regex);
@@ -30,7 +70,7 @@ TCB.prototype.repost = function(tweet) {
       }
     }
   );
-}
+};
 
 if (module) {
   module.exports = TCB;
